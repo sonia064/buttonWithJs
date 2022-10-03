21cf15370dab12c5152108770e02d59aad797adb
Sonia Akter <109096109+sonia064@users.noreply.github.com>
2022-10-04 01:47:18 +0600

    Add files via upload

---
+++ buttonJS.js
@@ -0,0 +1,39 @@
+         //1st code
+         
+         function buutton1()
+         {
+               //alert("Button is responsive now!!!");
+               document.getElementById("paragraph1").innerHTML = "Sonia Akter";
+         }
+         function buutton2()
+         {
+              // alert("Also 2nd Button is responsive now!!!");
+              document.getElementById("paragraph2").innerHTML = "Unmarried";
+         }
+         function buutton3()
+         {
+              // alert("Also 2nd Button is responsive now!!!");
+              document.getElementById("paragraph3").innerHTML = "22";
+         }
+         function buutton4()
+         {
+              // alert("Also 2nd Button is responsive now!!!");
+              document.getElementById("paragraph4").innerHTML = "2-Jun-2024";
+         }
+
+
+
+
+
+
+
+         //2nd code
+
+         function buutton11()
+         {
+               //alert("Button is responsive now!!!");
+               document.getElementById("paragraph11").innerHTML = "Sonia Akter";
+               document.getElementById("paragraph12").innerHTML = "Unmarried";
+               document.getElementById("paragraph13").innerHTML = "22";
+               document.getElementById("paragraph14").innerHTML = "2-Jun-2022";
+         }
